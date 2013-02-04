d8bc1e6e10af40f6afc6e4286025c5daa7b834c5
caseywatts <casey.s.watts@gmail.com>
2013-02-04 17:16:12 -0500

    modified it to accept a csv

--- roster.rb
+++ roster.rb
@@ -1,13 +1,32 @@
 require 'rubygems'
-require 'bundler/setup'
-require 'schema'
-require 'mysql'
+require 'csv'
+
+FILE_PATH = "roster.csv"
+
+#require 'bundler/setup'
+#require 'schema'
+#require 'mysql'
+
+arr = CSV.read("roster.csv", {:headers => true})
 
 queues = %w(BK BR CC DC ES JE MC PC SM SY TD TC Grad1 Grad2 OC1 OC2)
-colleges = %w(BK BR CC DC ES JE MC PC SM SY TD TC)
+#colleges = %w(BK BR CC DC ES JE MC PC SM SY TD TC)
 command_list = []
 mailman_command = "sudo /usr/local/mailman/bin/sync_members -f"
 
+queues.each do |queue|
+  emails = []
+  queue_list = arr.find_all {|row| row["Queue"] == queue}.each do |row|
+    emails << row["Yale Email"]
+  end
+    emails << "yahor.buben@yale.edu"
+    emails << "derek.zhao@yale.edu"
+    emails << "casey.watts@yale.edu"
+    filename = "studtech-st-#{queue.downcase}"
+    File.open(filename, 'w') {|f| f.write(emails.join("\n")) }
+    command_list << "#{mailman_command} #{filename} #{filename}"
+    puts queue
+end
 
 
 #Generate kilroy email list
@@ -32,17 +51,17 @@ mailman_command = "sudo /usr/local/mailman/bin/sync_members -f"
 # end
 
 #Generate 'all' email list
-queues.each do |queue|
-  users = User.find(:all, :conditions => ["college = ? OR queue = ?", queue, queue])
-  emails = users.map(&:email).uniq
-  emails << "decal@yale.edu"
-  if colleges.include? queue
-    emails << "yahor.buben@yale.edu"
-  end
-  filename = "studtech-st-#{queue.downcase}"
-  File.open(filename, 'w') {|f| f.write(emails.join("\n")) }
-  command_list << "#{mailman_command} #{filename} #{filename}"
-end
+#queues.each do |queue|
+  #users = User.find(:all, :conditions => ["college = ? OR queue = ?", queue, queue])
+  #emails = users.map(&:email).uniq
+  #emails << "decal@yale.edu"
+  #if colleges.include? queue
+    #emails << "yahor.buben@yale.edu"
+  #end
+  #filename = "studtech-st-#{queue.downcase}"
+  #File.open(filename, 'w') {|f| f.write(emails.join("\n")) }
+  #command_list << "#{mailman_command} #{filename} #{filename}"
+#end
 
 File.open("command_list", 'w') {|f| f.write("#!/bin/bash\n")}
 File.open("command_list", 'a') {|f| f.write(command_list.join("\n"))}
